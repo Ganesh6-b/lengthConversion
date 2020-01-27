9a14168c8f120dc50ef1649f5b29ca751a2bc8fd
Ganesh6-b <60285105+Ganesh6-b@users.noreply.github.com>
2020-01-27 12:20:43 +0530

    Create lengthConversion.py

---
+++ lengthConversion.py
@@ -0,0 +1,432 @@
+class mile:
+    def __init__(self,mile):
+        self.mile = mile
+    def Km(self):
+        Km = self.mile * 0.621371
+        print("{} mile in Kilometre is :".format(self.mile),Km)
+    def metre(self):
+        metre = self.mile * 1609.34
+        print("{} mile in metre is :".format(self.mile),metre)
+    def centimetre(self):
+        centimetre = self.mile * 160934
+        print("{} mile in centimetre is :".format(self.mile),centimetre)
+    def millimetre(self):
+        millimetre = self.mile * 1.609e+6
+        print("{} mile in millimetre is :".format(self.mile),millimetre)
+    def micrometre(self):
+        micrometre = self.mile * 1.609e+9
+        print("{} mile in micrometre is :".format(self.mile),micrometre)
+    def nanometre(self):
+        nanometre = self.mile * 1.609e+12
+        print("{} mile in nanometre is :".format(self.mile),nanometre)
+    def yard(self):
+        yard = self.mile * 1760
+        print("{} mile in yard is :".format(self.mile),yard)
+    def foot(self):
+        foot = self.mile * 1.609e+12
+        print("{} mile in foot is :".format(self.mile),foot)
+    def inch(self):
+        inch = self.mile * 1.609e+12
+        print("{} mile in inch is :".format(self.mile),inch)
+        
+class km:
+    def __init__(self,km):
+        self.km = km 
+    def mile(self):
+        mile = self.km * 0.621371
+        print("{} km in mile is :".format(self.km),mile)
+    def metre(self):
+        metre = self.km * 1000
+        print("{} km in metre is :".format(self.km),metre)
+    def centimetre(self):
+        centimetre = self.km * 100000
+        print("{} km in centimetre is :".format(self.km),centimetre)
+    def millimetre(self):
+        millimetre = self.km * 1e+6
+        print("{} km in millimetre is :".format(self.km),millimetre)
+    def micrometre(self):
+        micrometre = self.km * 1e+9
+        print("{} km in micrometre is :".format(self.km),micrometre)
+    def nanometre(self):
+        nanometre = self.km * 1e+12
+        print("{} km in nanometre is :".format(self.km),nanometre)
+    def yard(self):
+        yard = self.km * 1093.61
+        print("{} km in yard is :".format(self.km),yard)
+    def foot(self):
+        foot = self.km * 3280.84
+        print("{} km in foot is :".format(self.km),foot)
+    def inch(self):
+        inch = self.km * 39370.1
+        print("{} km in inch is :".format(self.km),inch) 
+
+class m:
+    def __init__(self,m):
+        self.m = m 
+    def mile(self):
+        mile = self.m * 0.000621371
+        print("{} m in mile is :".format(self.m),mile)
+    def kilometre(self):
+        kilometre = self.m * 0.001
+        print("{} m in Kilometre is :".format(self.m),kilometre)
+    def centimetre(self):
+        centimetre = self.m * 100
+        print("{} m in centimetre is :".format(self.m),centimetre)
+    def millimetre(self):
+        millimetre = self.m * 1000
+        print("{} m in millimetre is :".format(self.m),millimetre)
+    def micrometre(self):
+        micrometre = self.m * 1e+6
+        print("{} m in micrometre is :".format(self.m),micrometre)
+    def nanometre(self):
+        nanometre = self.m * 1e+9
+        print("{} m in nanometre is :".format(self.m),nanometre)
+    def yard(self):
+        yard = self.m * 1.09361
+        print("{} m in yard is :".format(self.m),yard)
+    def foot(self):
+        foot = self.m * 3.28084
+        print("{} m in foot is :".format(self.m),foot)
+    def inch(self):
+        inch = self.m * 39.3701
+        print("{} m in inch is :".format(self.m),inch) 
+
+class cm:
+    def __init__(self,cm):
+        self.cm = cm 
+    def mile(self):
+        mile = self.cm * 6.2137e-6
+        print("{} cm in mile is :".format(self.cm),mile)
+    def kilometre(self):
+        kilometre = self.cm * 1e-5
+        print("{} cm in Kilometre is :".format(self.cm),kilometre)
+    def metre(self):
+        metre = self.cm * 0.01
+        print("{} cm in metre is :".format(self.cm),metre)
+    def millimetre(self):
+        millimetre = self.cm * 10
+        print("{} cm in millimetre is :".format(self.cm),millimetre)
+    def micrometre(self):
+        micrometre = self.cm * 10000
+        print("{} cm in micrometre is :".format(self.cm),micrometre)
+    def nanometre(self):
+        nanometre = self.cm * 1e+7
+        print("{} cm in nanometre is :".format(self.cm),nanometre)
+    def yard(self):
+        yard = self.cm * 0.0109361
+        print("{} cm in yard is :".format(self.cm),yard)
+    def foot(self):
+        foot = self.cm * 0.0328084
+        print("{} cm in foot is :".format(self.cm),foot)
+    def inch(self):
+        inch = self.cm * 0.393701
+        print("{} cm in inch is :".format(self.cm),inch)
+
+class mm:
+    def __init__(self,mm):
+        self.mm = mm 
+    def mile(self):
+        mile = self.mm * 6.2137e-7
+        print("{} mm in mile is :".format(self.mm),mile)
+    def kilometre(self):
+        kilometre = self.mm * 1e-6
+        print("{} mm in Kilometre is :".format(self.mm),kilometre)
+    def metre(self):
+        metre = self.mm * 0.001
+        print("{} mm in metre is :".format(self.mm),metre)
+    def centimetre(self):
+        centimetre = self.mm * 0.1
+        print("{} mm in centimetre is :".format(self.mm),centimetre)
+    def micrometre(self):
+        micrometre = self.mm * 1000
+        print("{} mm in micrometre is :".format(self.mm),micrometre)
+    def nanometre(self):
+        nanometre = self.mm * 1e+6
+        print("{} mm in nanometre is :".format(self.mm),nanometre)
+    def yard(self):
+        yard = self.mm * 0.00109361
+        print("{} mm in yard is :".format(self.mm),yard)
+    def foot(self):
+        foot = self.mm * 0.00328084
+        print("{} mm in foot is :".format(self.mm),foot)
+    def inch(self):
+        inch = self.mm * 0.0393701
+        print("{} mm in inch is :".format(self.mm),inch)
+        
+class mcm:
+    def __init__(self,mcm):
+        self.mcm = mcm 
+    def mile(self):
+        mile = self.mcm * 6.2137e-10
+        print("{} mcm in mile is :".format(self.mcm),mile)
+    def kilometre(self):
+        kilometre = self.mcm * 1e-9
+        print("{} mcm in Kilometre is :".format(self.mcm),kilometre)
+    def metre(self):
+        metre = self.mcm * 1e-6
+        print("{} mcm in metre is :".format(self.mcm),metre)
+    def centimetre(self):
+        centimetre = self.mcm * 1e-4
+        print("{} mcm in centimetre is :".format(self.mcm),centimetre)
+    def millimetre(self):
+        millimetre = self.mcm * 0.001
+        print("{} mcm in millimetre is :".format(self.mcm),millimetre)
+    def nanometre(self):
+        nanometre = self.mcm * 1000
+        print("{} mcm in nanometre is :".format(self.mcm),nanometre)
+    def yard(self):
+        yard = self.mcm * 1.0936e-6
+        print("{} mcm in yard is :".format(self.mcm),yard)
+    def foot(self):
+        foot = self.mcm * 3.2808e-6
+        print("{} mcm in foot is :".format(self.mcm),foot)
+    def inch(self):
+        inch = self.mcm * 3.937e-5
+        print("{} mcm in inch is :".format(self.mcm),inch)
+        
+class nn:
+    def __init__(self,nn):
+        self.nn = nn 
+    def mile(self):
+        mile = self.nn * 6.2137e-13
+        print("{} nn in mile is :".format(self.nn),mile)
+    def kilometre(self):
+        kilometre = self.nn * 1e-12
+        print("{} nn in Kilometre is :".format(self.nn),kilometre)
+    def metre(self):
+        metre = self.nn * 1e-9
+        print("{} nn in metre is :".format(self.nn),metre)
+    def centimetre(self):
+        centimetre = self.nn * 1e-7
+        print("{} nn in centimetre is :".format(self.nn),centimetre)
+    def millimetre(self):
+        millimetre = self.nn * 1e-6
+        print("{} nn in millimetre is :".format(self.nn),millimetre)
+    def micrometre(self):
+        micrometre = self.nn * 0.001
+        print("{} nn in micrometre is :".format(self.nn),micrometre)
+    def yard(self):
+        yard = self.nn * 1.0936e-9
+        print("{} nn in yard is :".format(self.nn),yard)
+    def foot(self):
+        foot = self.nn * 3.2808e-9
+        print("{} nn in foot is :".format(self.nn),foot)
+    def inch(self):
+        inch = self.nn * 3.937e-8
+        print("{} nn in inch is :".format(self.nn),inch)
+
+class yd:
+    def __init__(self,yd):
+        self.yd = yd 
+    def mile(self):
+        mile = self.yd * 0.000568182
+        print("{} yd in mile is :".format(self.yd),mile)
+    def kilometre(self):
+        kilometre = self.yd * 0.0009144
+        print("{} yd in Kilometre is :".format(self.yd),kilometre)
+    def metre(self):
+        metre = self.yd * 0.9144
+        print("{} yd in metre is :".format(self.yd),metre)
+    def centimetre(self):
+        centimetre = self.yd * 91.44
+        print("{} yd in centimetre is :".format(self.yd),centimetre)
+    def millimetre(self):
+        millimetre = self.yd * 914.4
+        print("{} yd in millimetre is :".format(self.yd),millimetre)
+    def micrometre(self):
+        micrometre = self.yd * 914400
+        print("{} yd in micrometre is :".format(self.yd),micrometre)
+    def nanometre(self):
+        nanometre = self.yd * 9.144e+8
+        print("{} yd in nanometre is :".format(self.yd),nanometre)
+    def foot(self):
+        foot = self.yd * 3
+        print("{} yd in foot is :".format(self.yd),foot)
+    def inch(self):
+        inch = self.yd * 36
+        print("{} yd in inch is :".format(self.yd),inch)
+
+class ft:
+    def __init__(self,ft):
+        self.ft = ft 
+    def mile(self):
+        mile = self.ft * 0.000189394
+        print("{} ft in mile is :".format(self.ft),mile)
+    def kilometre(self):
+        kilometre = self.ft * 0.0003048
+        print("{} ft in Kilometre is :".format(self.ft),kilometre)
+    def metre(self):
+        metre = self.ft * 0.3048
+        print("{} ft in metre is :".format(self.ft),metre)
+    def centimetre(self):
+        centimetre = self.ft * 30.48
+        print("{} ft in centimetre is :".format(self.ft),centimetre)
+    def millimetre(self):
+        millimetre = self.ft * 304.8
+        print("{} ft in millimetre is :".format(self.ft),millimetre)
+    def micrometre(self):
+        micrometre = self.ft * 304800
+        print("{} ft in micrometre is :".format(self.ft),micrometre)
+    def nanometre(self):
+        nanometre = self.ft * 3.048e+8
+        print("{} ft in nanometre is :".format(self.ft),nanometre)
+    def yard(self):
+        yard = self.ft * 0.333333
+        print("{} ft in yard is :".format(self.ft),yard)
+    def inch(self):
+        inch = self.ft * 12
+        print("{} ft in inch is :".format(self.ft),inch)
+        
+class inch:
+    def __init__(self,inch):
+        self.inch = inch 
+    def mile(self):
+        mile = self.inch * 0.000189394
+        print("{} inch in mile is :".format(self.inch),mile)
+    def kilometre(self):
+        kilometre = self.inch * 0.0003048
+        print("{} inch in Kilometre is :".format(self.inch),kilometre)
+    def metre(self):
+        metre = self.inch * 0.3048
+        print("{} inch in metre is :".format(self.inch),metre)
+    def centimetre(self):
+        centimetre = self.inch * 30.48
+        print("{} inch in centimetre is :".format(self.inch),centimetre)
+    def millimetre(self):
+        millimetre = self.inch * 304.8
+        print("{} inch in millimetre is :".format(self.inch),millimetre)
+    def micrometre(self):
+        micrometre = self.inch * 304800
+        print("{} inch in micrometre is :".format(self.inch),micrometre)
+    def nanometre(self):
+        nanometre = self.inch * 3.048e+8
+        print("{} inch in nanometre is :".format(self.inch),nanometre)
+    def yard(self):
+        yard = self.inch * 0.333333
+        print("{} inch in yard is :".format(self.inch),yard)
+    def inch(self):
+        foot = self.inch * 12
+        print("{} inch in foot is :".format(self.inch),foot)
+
+print("Conversions available are 1 : miles, 2 : kilometres, 3 : metres, 4 : centimetres, 5 : millimetres, 6 : micrometre, 7 : nanometres, 8 : yards,9 : foots, 10 : inchs")
+conv1 = input("Enter you conversion from:")
+if conv1=="miles":
+    val = int(input("Enter your value:"))
+    miles =mile(val)
+    miles.kilometre()
+    miles.metre()
+    miles.centimetre()
+    miles.millimetre()
+    miles.micrometre()
+    miles.nanometre()
+    miles.yard()
+    miles.foot()
+    miles.inch()
+elif conv1=="kilometres":
+    val = int(input("Enter your value:"))
+    kilometres = km(val)
+    kilometres.mile()
+    kilometres.metre()
+    kilometres.centimetre()
+    kilometres.millimetre()
+    kilometres.micrometre()
+    kilometres.nanometre()
+    kilometres.yard()
+    kilometres.foot()
+    kilometres.inch()
+elif conv1=="metres":
+    val = int(input("Enter your value:"))
+    metres = m(val)
+    metres.mile()
+    metres.kilometre()
+    metres.centimetre()
+    metres.millimetre()
+    metres.micrometre()
+    metres.nanometre()
+    metres.yard()
+    metres.foot()
+    metres.inch()
+elif conv1=="centimetres":
+    val = int(input("Enter your value:"))
+    centimetres = cm(val)
+    centimetres.mile()
+    centimetres.kilometre()
+    centimetres.metre()
+    centimetres.millimetre()
+    centimetres.micrometre()
+    centimetres.nanometre()
+    centimetres.yard()
+    centimetres.foot()
+    centimetres.inch()
+elif conv1=="millimetres":
+    val = int(input("Enter your value:"))
+    millimetres = mm(val)
+    millimetres.mile()
+    millimetres.kilometre()
+    millimetres.metre()
+    millimetres.centemetre()
+    millimetres.micrometre()
+    millimetres.nanometre()
+    millimetres.yard()
+    millimetres.foot()
+    millimetres.inch()
+elif conv1=="micrometres":
+    val = int(input("Enter your value:"))
+    micrometres = mcm(val)
+    micrometres.mile()
+    micrometres.kilometre()
+    micrometres.metre()
+    micrometres.centemetre()
+    micrometres.millimetre()
+    micrometres.nanometre()
+    micrometres.yard()
+    micrometres.foot()
+    micrometres.inch()
+elif conv1=="nanometres":
+    val = int(input("Enter your value:"))
+    nanometres = nn(val)
+    nanometres.mile()
+    nanometres.kilometre()
+    nanometres.metre()
+    nanometres.centemetre()
+    nanometres.millimetre()
+    nanometres.micrometre()
+    nanometres.yard()
+    nanometres.foot()
+    nanometres.inch()
+elif conv1=="yards":
+    val = int(input("Enter your value:"))
+    yards = yd(val)
+    yards.mile()
+    yards.kilometre()
+    yards.metre()
+    yards.centemetre()
+    yards.millimetre()
+    yards.micrometre()
+    yards.nanometre()
+    yards.foot()
+    yards.inch()
+elif conv1=="foots":
+    val = int(input("Enter your value:"))
+    foots = ft(val)
+    foots.mile()
+    foots.kilometre()
+    foots.metre()
+    foots.centemetre()
+    foots.millimetre()
+    foots.micrometre()
+    foots.nanometre()
+    foots.yard()
+    foots.inch()
+elif conv1=="inchs":
+    val = int(input("Enter your value:"))
+    inchs = inch(val)
+    inchs.mile()
+    inchs.kilometre()
+    inchs.metre()
+    inchs.centemetre()
+    inchs.millimetre()
+    inchs.micrometre()
+    inchs.nanometre()
+    inchs.yard()
+    inchs.foot()
